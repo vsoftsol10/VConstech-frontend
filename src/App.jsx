946fb99e4ea4bd95630d3e870f095aedf27f534e
Nirmal <nirmalsep2001@gmail.com>
2025-08-19 13:39:25 +0530

    Add Site Engineer page created

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import AuthLayout from '../src/layouts/AuthLayout';
 import ContractorDashboard from './pages/contractor/ContractorDashboard';
 import { Route, Routes } from 'react-router-dom';
+import AddSiteEngg from './pages/contractor/AddSiteEngg';
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<AuthLayout/>}/>
       <Route path='/contractorDashboard' element={<ContractorDashboard/>} />
+      <Route path='/add-site-engg' element={<AddSiteEngg/>} />
     </Routes>
   )
 }
