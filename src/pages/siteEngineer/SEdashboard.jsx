2b1186b8a1f43bd7237e60d97063f2eaa611c885
vigneshwaran <vigneshwaran274mail@gmail.com>
2025-10-08 13:50:58 +0530

    done

--- src/pages/siteEngineer/SEdashboard.jsx
+++ src/pages/siteEngineer/SEdashboard.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import SiteNavbar from '../../components/siteEngineer/SiteNavbar'
 
 const SEdashboard = () => {
   return (
-    <div>
-        <SiteNavbar/>
-       <h1 className='my-25 text-center'> SEdashboard </h1> 
-    </div>
+    <div>SEdashboard</div>
   )
 }
 
